74fb60c0091ba725aa7efe5fcb3ecf0346bd748e
Stacy Mackin <stacy.mackin@gmail.com>
2020-11-15 21:14:30 -0500

    Add Netlify Identity Widget

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import netlifyIdentity from 'netlify-identity-widget';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    netlifyIdentity.init({
+      locale: 'en', // defaults to 'en'
+    });
   }
-
 }

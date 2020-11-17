7f48387a3b00bd185323787fbaaf678970dfec24
Stacy Mackin <stacy.mackin@gmail.com>
2020-11-16 21:01:21 -0500

    Add twilio call to ring-doorbell.js

--- functions/ring-doorbell.js
+++ functions/ring-doorbell.js
@@ -1,6 +1,16 @@
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 exports.handler = async function(event, context) {
-  return {
-      statusCode: 200,
-      body: JSON.stringify(event)
-  };
+  return client.messages
+  .create({
+     body: 'Ding dong',
+     from: '+15177973455',
+     to: JSON.parse(event.body.number)
+   })
+  .then(message => ({
+    statusCode: 200,
+    body: message.sid
+  }))
 }

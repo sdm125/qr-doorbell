c9572dd99f35e628856b7211ce70b5e993b0e5fa
Stacy Mackin <stacy.mackin@gmail.com>
2020-11-17 20:11:41 -0500

    Remove settings and history components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DoorbellComponent } from './components/doorbell/doorbell.component';
-import { HistoryComponent } from './components/history/history.component';
 import { HomeComponent } from './components/home/home.component';
-import { SettingsComponent } from './components/settings/settings.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'history', component: HistoryComponent },
   { path: 'doorbell/:number', component: DoorbellComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { HistoryComponent } from './components/history/history.component';
 import { FormsModule } from '@angular/forms';
 import { AddPhoneNumberComponent } from './components/add-phone-number/add-phone-number.component';
 import { DoorbellComponent } from './components/doorbell/doorbell.component';
@@ -15,8 +13,6 @@ import { DoorbellComponent } from './components/doorbell/doorbell.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    SettingsComponent,
-    HistoryComponent,
     AddPhoneNumberComponent,
     DoorbellComponent,
   ],
